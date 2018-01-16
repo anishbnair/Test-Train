667f8b5afcafd5de2684eaf15b902f76ee0cf874
anishbnair <nbanish@gmail.com>
2018-01-16 17:00:28 -0500

    googlebutton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,20 @@
-// function onSuccess(googleUser) {
-//     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-// }
-// function onFailure(error) {
-//     console.log(error);
-// }
-// function renderButton() {
-//     gapi.signin2.render('my-signin2', {
-//         'scope': 'profile email',
-//         'width': 240,
-//         'height': 50,
-//         'longtitle': true,
-//         'theme': 'dark',
-//         'onsuccess': onSuccess,
-//         'onfailure': onFailure
-//     });
-// }
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
 
 
 // Initialize Firebase
@@ -94,7 +94,7 @@ var keyToUpdate = '';
 
 // Function to clear form contents
 function clearForm(event) {
-    // event.preventDefault();
+    event.preventDefault();
     $("#trainName").val('');
     $("#trainDestination").val('');
     $("#trainTime").val('');
